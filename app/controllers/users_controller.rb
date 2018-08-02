06ae8a309874a34f10c28fadc1dcab9a838757b6
AyarosF <sorayafructuoso@gmail.com>
2018-08-02 21:19:06 +0200

    modif edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,17 +29,17 @@ class UsersController < ApplicationController
   end
 
   def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 
   def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
-  end    
+  end
 
   def create
     @user = User.new(user_params)
