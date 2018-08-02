d183320bc3119b75d2743a723fb6f111f148ce8d
Hugo Payet <hugopayetsync@gmail.com>
2018-08-02 16:14:55 +0200

    log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,44 @@ class UsersController < ApplicationController
   end
 
   def show
+    @username = User.find(params[:id]).username
+    @email = User.find(params[:id]).email
   end
 
   def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #log_in @user
+      #flash[:success] = "Salut salut !"
+      redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+    end
+
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    
+  
   end
 
   def destroy
